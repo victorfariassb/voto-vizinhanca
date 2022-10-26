69fdfccc650937affc9c58536529787faed31f92
Victor Farias <victorfariassb@gmail.com>
2022-10-26 16:26:45 -0300

    update

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ dados = [
         "cidade": "Rio de Janeiro",
         "local": 'Colégio Santa Maria',
         "zona": 1,
-        "secao" : 1,
+        "secao": 1,
         "Bolsonaro": 12,
         "Lula": 11
     },
@@ -13,15 +13,18 @@ dados = [
         "cidade": "Rio de Janeiro",
         "local": 'Colégio Santa Catarina',
         "zona": 1,
-        "secao" : 2,
+        "secao": 2,
         "Bolsonaro": 11,
-        "Lula": 12
+        "Lula": 40
     }
 ]
 
-let zona_input = document.getElementById('zona_input')
 let secao_input = document.getElementById('secao_input')
+let zona_input = document.getElementById('zona_input')
+
+let botao = document.getElementById('buscar')
 let resposta = document.querySelector('output')
+let resultado = document.querySelector('.resultado')
 
 let estado = document.querySelector('.estado')
 let cidade = document.querySelector('.cidade')
@@ -29,50 +32,42 @@ let bairro = document.querySelector('.bairro')
 let local = document.querySelector('.local')
 let zona_secao = document.querySelector('.zona_secao')
 let votos = document.querySelector('.votos')
-let foto = document.querySelector('.foto')
 
 let body = document.querySelector('body')
 
-zona_input.addEventListener('input', validar)
-secao_input.addEventListener('input', validar)
+buscar.addEventListener('click', validar(secao_input, zona_input))
 
-function validar() {
-    let valor = zona_input.value.trim();
-    let valor2 = secao_input.value.trim();
+function validar(v1, v2) {
 
-    resultados = []
+    valor = v1.value.trim();
+    valor2 = v2.value.trim();
 
     for (let dado of dados) {
         if (valor == dado.zona && valor2 == dado.secao) {
+            voto_grafico = {}
+            voto_grafico = {
+                'Lula (PT)': dado.Lula,
+                'Bolsonaro (PL)': dado.Bolsonaro
+            }
+            resultado.style.border = "1px solid black"
+            resultado.style.backgroundColor = "rgb(198, 198, 198)"
             estado.innerHTML = "<p><b>Estado:</b> " + dado.estado + "</p>"
             cidade.innerHTML = "<p><b>Cidade:</b> " + dado.cidade + "</p>"
             local.innerHTML = "<p><b>Local de votação:</b> " + dado.local + "</p>"
             zona_secao.innerHTML = "<p><b>Zona:</b> " + dado.zona + "<b>   Seção:</b> " + dado.secao + "</p>"
             if (dado.Bolsonaro > dado.Lula) {
                 body.style.backgroundColor = "#30306D"
-                votos.innerHTML = "<h3>Bolsonaro ganhou com " + dado.Bolsonaro + " votos. Lula teve " + dado.Lula + " votos.<h3>"
-                foto.innerHTML =  "<img src='bolsonaro.png' alt='Lula'>"
+                votos.innerHTML = "<h3>Bolsonaro ganhou com " + dado.Bolsonaro + " votos. <br>" + "Lula teve " + dado.Lula + " votos.<h3>"
             }
             if (dado.Bolsonaro < dado.Lula) {
                 body.style.backgroundColor = "#C0122D"
                 votos.innerHTML = "<h3>Lula ganhou com " + dado.Lula + " votos. Bolsonaro teve " + dado.Bolsonaro + " votos.<h3>"
-                foto.innerHTML =  "<img src='lula2.png' alt='Lula'>"
             }
             if (dado.Bolsonaro == dado.Lula) {
                 body.style.backgroundColor = "rgb(210, 207, 207)"
                 votos.innerHTML = "<h3>Os dois candidatos empataram com " + dado.Lula + " votos.<h3>"
-            
             }
+        
         }
     }
-}
-
-function limpar() {
-    estado.textContent = ''
-    cidade.textContent = ''
-    local.textContent = ''
-    zona_secao.textContent = ''
-    votos.textContent = ''
-
-    body.style.backgroundColor = "rgb(210, 207, 207)"
 }
