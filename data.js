d4f4c29c14ab5e42688ca6b99134d36adde325f2
victorfariassb <victorfariassb@gmail.com>
2022-10-28 12:44:56 -0300

    update js

--- data.js
+++ data.js
@@ -1,23 +1,9 @@
-let dados = [
-    {
-        "estado": "Rio de Janeiro",
-        "cidade": "Rio de Janeiro",
-        "local": 'Colégio Santa Maria',
-        "zona": 1,
-        "secao": 1,
-        "Bolsonaro": 12,
-        "Lula": 11
-    },
-    {
-        "estado": "Rio de Janeiro",
-        "cidade": "Rio de Janeiro",
-        "local": 'Colégio Santa Catarina',
-        "zona": 1,
-        "secao": 2,
-        "Bolsonaro": 11,
-        "Lula": 40
-    }
-]
+
+let dados = []
+
+fetch("votos_secao.json")
+  .then(response => response.json())
+  .then(json => dados.push(json));
 
 let zona_input = document.getElementById('zona_input')
 let secao_input = document.getElementById('secao_input')
@@ -45,51 +31,52 @@ function validar() {
     let valor = Number(zona_input.value.trim());
     let valor2 = Number(secao_input.value.trim());
 
-    for (let dado of dados) {
-        if (valor == dado.zona && valor2 == dado.secao) {
-            check += 1
-            votos_candidadtos = []
-            votos_candidadtos.push(dado.Lula, dado.Bolsonaro)
-            votos.style.paddingBottom = '1vh'
-            resultado.style.border = "1px solid black"
+    for (let local of dados){ 
+        for (let dado of local) {
+            if (valor == dado.zona && valor2 == dado.secao) {
+                check += 1
+                votos_candidadtos = []
+                votos_candidadtos.push(dado.Lula, dado.Bolsonaro)
+                votos.style.paddingBottom = '1vh'
+                resultado.style.border = "1px solid black"
 
-            estado.innerHTML = "<p><b>Estado:</b> " + dado.estado + "</p>"
-            cidade.innerHTML = "<p><b>Cidade:</b> " + dado.cidade + "</p>"
-            local.innerHTML = "<p><b>Local de votação:</b> " + dado.local + "</p>"
-            zona_secao.innerHTML = "<p><b>Zona:</b> " + dado.zona + "<b>   Seção:</b> " + dado.secao + "</p>"
-            if (dado.Bolsonaro > dado.Lula) {
-                votos.style.color = 'black'
-                votos.innerHTML = "<h3>Bolsonaro ganhou com " + dado.Bolsonaro + " votos. Lula teve " + dado.Lula + " votos.<h3>"
+                estado.innerHTML = "<p><b>Estado:</b> " + dado.estado + "</p>"
+                cidade.innerHTML = "<p><b>Cidade:</b> " + dado.municipio + "</p>"
+                local.innerHTML = "<p><b>Local de votação:</b> " + dado.local + "</p>"
+                zona_secao.innerHTML = "<p><b>Zona:</b> " + dado.zona + "<b>   Seção:</b> " + dado.secao + "</p>"
+                if (dado.Bolsonaro > dado.Lula) {
+                    votos.style.color = 'black'
+                    votos.innerHTML = "<h3>Bolsonaro ganhou com " + dado.Bolsonaro + " votos. Lula teve " + dado.Lula + " votos.<h3>"
+                }
+                else if (dado.Bolsonaro < dado.Lula) {
+                    votos.style.color = 'black'
+                    votos.innerHTML = "<h3>Lula ganhou com " + dado.Lula + " votos. Bolsonaro teve " + dado.Bolsonaro + " votos.<h3>"
+                }
+                else if (dado.Bolsonaro == dado.Lula) {
+                    votos.style.color = 'black'
+                    votos.innerHTML = "<h3>Os dois candidatos empataram com " + dado.Lula + " votos.<h3>"
+                }
+                grafico.style.display = 'block'
+                grafico.style.height = '250px'
+                grafico.style.width = '500px'
+                myChart.data.datasets[0].data[0] = votos_candidadtos[0];
+                myChart.data.datasets[1].data[0] = votos_candidadtos[1];
+                myChart.update();
+                break
             }
-            else if (dado.Bolsonaro < dado.Lula) {
+            else if (valor != dado.zona || valor2 != dado.secao) {
                 votos.style.color = 'black'
-                votos.innerHTML = "<h3>Lula ganhou com " + dado.Lula + " votos. Bolsonaro teve " + dado.Bolsonaro + " votos.<h3>"
+                resultado.style.border = "none"
+                votos.innerHTML = "<h3>Local não identificado. Tente novamente</h3>"
+                estado.innerHTML = ""
+                cidade.innerHTML = ""
+                local.innerHTML = ""
+                zona_secao.innerHTML = ""
+                grafico.style.display = 'none'
             }
-            else if (dado.Bolsonaro == dado.Lula) {
-                votos.style.color = 'black'
-                votos.innerHTML = "<h3>Os dois candidatos empataram com " + dado.Lula + " votos.<h3>"
             }
-            grafico.style.display = 'block'
-            grafico.style.height = '250px'
-            grafico.style.width = '500px'
-            myChart.data.datasets[0].data[0] = votos_candidadtos[0];
-            myChart.data.datasets[1].data[0] = votos_candidadtos[1];
-            myChart.update();
-            break
-        }
-        else if (valor != dado.zona || valor2 != dado.secao) {
-            votos.style.color = 'black'
-            resultado.style.border = "none"
-            votos.innerHTML = "<h3>Local não identificado. Tente novamente</h3>"
-            estado.innerHTML = ""
-            cidade.innerHTML = ""
-            local.innerHTML = ""
-            zona_secao.innerHTML = ""
-            grafico.style.display = 'none'
-
         }
     }
-}
 
 
 const data = {
